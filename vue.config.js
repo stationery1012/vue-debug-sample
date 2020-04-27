e0cf717e4211e2a6fc1844378e9a653dbfde6b24
stationery1012 <64267605+stationery1012@users.noreply.github.com>
2020-04-27 22:09:27 +0900

    不要コードの削除

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
-var openInEditor = require('launch-editor-middleware')
-
 module.exports = {
   configureWebpack: {
     devtool: 'source-map',
-  },
-  devServer: {
-    before(app) {
-      console.log('before')
-      app.use('/__open-in-editor', openInEditor('code'))
-    },
   },
 }
